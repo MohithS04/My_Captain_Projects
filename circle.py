87fb517b1859ce49ed3095f5789846308eaa1720
MohithS04 <88337483+MohithS04@users.noreply.github.com>
2021-08-26 20:55:51 +0530

    Add files via upload

---
+++ circle.py
@@ -0,0 +1,8 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> from math import pi
+>>> r = float(input ("Enter radius of circle : "))
+Enter radius of circle : 1.1
+>>> print ("Area of the circle is: " + str(pi * r**2))
+Area of the circle is: 3.8013271108436504
+>>> 
